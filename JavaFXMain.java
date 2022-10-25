17d3de9e643fcb8a815f6e40f7fa285abd1432cf
Rafi-Sutrisno <115334112+Rafi-Sutrisno@users.noreply.github.com>
2022-10-25 18:07:09 +0700

    Update JavaFXMain.java

--- JavaFXMain.java
+++ JavaFXMain.java
@@ -1,41 +1,52 @@
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
-public class JavaFXMain extends Application {
-    
-    @Override
-    public void start(Stage Test) {
-        //remove code from here 
-	Button btn = new Button();
-        Button exit  = new Button();
-        btn.setText("Hallo Siap Mulai");
-        exit.setText("Exit");
-        btn.setLayoutX(70);
-        btn.setLayoutY(70);
-        
-        exit.setLayoutX(100);
-        exit.setLayoutY(100);
-        if(btn.isPressed()){
-            System.out.println("Is Clicked");
-        }
-        btn.setOnAction(new EventHandler<ActionEvent>() { 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Clicked");
-            }
-        });
-        
-        Test.setTitle("Hello World!");
-        Test.show();
-    }
 
+public class JavaFXMain extends Application {
+
+    public void start(Stage primaryStage){
+        Button btn = new Button();
+        btn.setText("Start");
+        btn.setLayoutX(200);
+        btn.setLayoutY(100);
+        btn.setOnAction ((ActionEvent event) -> {
+            System.out.println("Hello Object Oriented!");
+        });
+        Button btn2 = new Button ();
+        btn2.setText("Exit");
+        btn2.setLayoutX(300);
+        btn2.setLayoutY(200);
+        btn2.setOnAction ((ActionEvent event) -> {
+            System.out.println("Keluar");
+        });
+        Group root = new Group();
+        root.getChildren().add(btn);
+        root.getChildren().add(btn2);
+        
+        HBox root2 = new HBox();
+        
+        for(int i=0; i<3; i++){
+            Button test = new Button ("Start");
+            root2.getChildren().add(test);
+        }
+        root2.setLayoutX(40);
+        root2.setLayoutY(400);
+        root.getChildren().add(root2);    
+        Scene scene = new Scene (root, 640, 480);
+        primaryStage.setTitle("Tugas PBO");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     public static void main(String[] args) {
         launch(args);
     }
 }
-
